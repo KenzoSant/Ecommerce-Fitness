2bf926eace42a80b6e48d183c388e8c3c07be55d
Victor Hugo Ferreira Marinho <victorhugoferreira452@gmail.com>
2024-06-15 09:42:55 -0300

    Add cors

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -1,9 +1,26 @@
 import express from 'express';
 import multer from 'multer';
 import path from 'path';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
+
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
